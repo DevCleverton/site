b07630863229dfc6415c1d5c5ebb71982185af3b
Cleverton da Rocha <56972170+DevCleverton@users.noreply.github.com>
2023-04-17 09:59:19 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
           {/* <Footer /> */}
         </UserContext>
       </BrowserRouter>
-    </><script>(function(w, d) { w.CollectId = "64385e012f45336a321656e9"; var h = d.head || d.getElementsByTagName("head")[0]; var s = d.createElement("script"); s.setAttribute("type", "text/javascript"); s.async=true; s.setAttribute("src", "https://collectcdn.com/launcher.js"); h.appendChild(s); })(window, document);</script>
+    </>
   );
 }
 
